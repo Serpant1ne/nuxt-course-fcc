dd5176270c2342ef5b6a5dfae00c0e90c84971eb
Serpant1ne <gartsmankonstantin@gmail.com>
2024-11-03 21:29:50 +0100

    added tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- <NuxtRouteAnnouncer /> -->
+    <!-- <NuxtWelcome /> -->
+    <h1 class="bg-red-500 text-4xl font-bold">Hello nuxt 3</h1>
+    <button class="bg-sky-500 border-none">dads</button>
   </div>
 </template>
